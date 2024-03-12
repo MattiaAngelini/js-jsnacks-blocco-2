496bb419a8477f28efca1c3b87c2560f8e85c23c
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-12 15:49:50 +0100

    Add: snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -11,10 +11,23 @@ const numbersContainer = [34,23,1,43,22,44,21,54,43];
 // creo due variabili per selezionare e poi assegnare style ai due div.
 const boxRed = document.querySelector("#box-red");
 boxRed.style.color = ("red");
-
 const boxGreen = document.querySelector("#box-green");
 boxGreen.style.color = ("green")
 
+        // Creo ciclo for per cercare nell'array 
+        //e assegno ad una costante il valore di quell'indice.
+        for (let i = 0; i < numbersContainer.length; i++) {
+            const number = numbersContainer[i];
 
+            // Verifico se il number che contiene valore indice (quindi numeri array),
+            // è pari o dispari e lo aggiungo con concatenazione innerHTML al div corrispondente.
+            // (aggiunge spazio per distanziare risultati in dom)
+            if (number % 2 === 0) {
+                boxGreen.innerHTML += number + " ";
+            } else {
+                boxRed.innerHTML += number + " ";
+            }
+        }
+        
 
 

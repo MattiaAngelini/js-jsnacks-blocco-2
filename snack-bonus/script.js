0891e186b933536c58d2ce33cf24eddb4ed52c57
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-12 16:13:06 +0100

    Add: first part bonus

---
+++ snack-bonus/script.js
@@ -0,0 +1,14 @@
+//- Chiedi un numero di 4 cifre all’utente 
+//e calcola la somma di tutte le cifre che compongono il numero.
+
+//Creo prompt per inserimento numero a 4 cifre
+const userNumber = prompt("Inserisci un numero a 4 cifre");
+
+//creo ciclo for per stampare ogni singolo numero inserito.
+for(let i = 0; i < userNumber.length; i++) {
+    console.log(userNumber[i]);
+} 
+
+
+
+

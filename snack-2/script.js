6052a9227218771e239d71313892eb7666b01059
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-13 09:56:47 +0100

    Add: fixed snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -8,14 +8,15 @@ const userWord2 = prompt("Inserisci un'altra parola");
 //Creo condizioni: se la lunghezza del testo inserito primo prompt
 //è più lungo del secondo stampo primo prompt, altrimenti se viceversa stampo
 //valore del secondo prompt, se infine uguali stampo stessa lunghezza ;
+
+let userMessage;
+
 if (userWord1.length < userWord2.length) {
-    console.log(userWord1);
-    console.log(userWord2);
+    userMessage = userWord1 + ' ' + userWord2;
 } else if (userWord1.length > userWord2.length) {
-    console.log(userWord2);
-    console.log(userWord1);
-} else  {
-    console.log("stessa lunghezza");
+    userMessage = userWord2 + ' ' + userWord1;
+} else {
+    userMessage = "stessa lunghezza";
 }
 
-
+console.log(userMessage);
